4cca415c09a84cd806df0f9647c8de87a095033a
BoomerDev <theboomerdev@gmail.com>
2022-05-13 13:46:24 +0200

    Incluir Plantilla para objetos

--- locales.functions.js
+++ locales.functions.js
@@ -4,11 +4,14 @@ const mBebida = require('./models/bebida.model')
 
 const get_locales = async (req, res) => {
 
+    // EXAMEN BASE DE DATOS
     const locales = await mLocales.find({}).sort({'nombre': 1})
 
+    // EXAMEN DE LENGUAJE DE MARCAS
     const data = {
+        title:"UN TITULO MOLON",
         locales:locales
-    }
+     }
     res.render('locales/list_locales', data);
 
 }
